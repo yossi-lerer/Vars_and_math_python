a1be61f1039c0ca9dfd29627f23b3f14f0b6c8c5
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-18 15:02:58 +0300

    full distance for a trip from base to target and back

--- mission_control.py
+++ mission_control.py
@@ -4,3 +4,6 @@ distance_to_target = 10.8
 mission_active_atatus = True
 print(f"agent name: {agent_name}, mission code: {mission_code}, distance to target: {distance_to_target}, mission active status: {mission_active_atatus}")
 print(f"type of all varable: agent_name {type(agent_name)}, mission_code: {type(mission_code)}, distance_to_target: {type(distance_to_target)}, mission_active_atatus: {type(mission_active_atatus)}.")
+
+travel_distance = distance_to_target * 2 
+print(f"full distance for a trip from base to target and back: {travel_distance}")
