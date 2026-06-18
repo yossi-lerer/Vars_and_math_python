04d291ee86bcf4f94b7c1ee98a4d0a0b49335e40
yossi-lerer <yossi.lerer.2003@gmail.com>
2026-06-18 14:34:11 +0300

    add the print data to console

--- mission_control.py
+++ mission_control.py
@@ -2,3 +2,4 @@ agent_name = "jj"
 mission_code = 118842
 distance_to_target = 10.8
 mission_active_atatus = True
+print(f"agent name: {agent_name}, mission code: {mission_code}, distance to target: {distance_to_target}, mission active status: {mission_active_atatus}")
